8451e01f623a471108acc6278afb4a87e2176b51
Taron Gukasyan <funkadelica@mail.ru>
2024-04-09 17:22:15 +0300

    first ever commit 3 methods(blank) + main

---
+++ src/main/java/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+    void first(){
+        System.out.println();
+    }
+
+    void second(){
+        System.out.println();
+    }
+
+    void third(){
+        System.out.println();
+    }
+}
