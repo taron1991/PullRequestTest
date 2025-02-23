d38ccbf1d545986d54479dd6aea8f64467345e13
David Gvaliya <d.gvalia@yandex.ru>
2025-02-23 12:17:53 +0300

    Update method main, branch devDavid

---
+++ src/main/java/Feachure.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Feachure {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("David", "M");
+        map.put("Kate", "W");
+        map.put("Petr", "M");
+        map.put("Nataly", "W");
+
+
+    }
+}
